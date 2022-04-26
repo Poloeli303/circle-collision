79f9ddceb45d83b468ecd279b0ed3ec83311c98f
Poloeli303 <91678247+Poloeli303@users.noreply.github.com>
2022-04-26 09:07:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import math
+import pygame
+import random
+pygame.init
+screen = pygame.display.set_mode((800,800))
+pygame.display.set_caption("circle collision")
+
+
 def CircleCollision(x1,x2,y1,y2, radius):
     if (math.sqrt((x2 - x1)^2 + (y2- y1)^2))<radius:
         return True
     else:
         return False
     
-print("Enter x position")
-x1=int(input())
-print("Enter second x position")
-x2=int(input())
-print("Enter y position")
-y1=int(input())
-print("Enter second y position")
-y2=int(input())
-print("Enter radius")
-radius=int(input())
+    #game variables
+doExit = False #variable to quit out of game loop
+clock = pygame.time.Clock() #sets up a game clock to regulate game spee
+while not doExit:
+    chief = random.randrange(1,250)
+    keef = random.randrange(1,250)
+    love = random.randrange(1,250)
+    sosa = random.randrange(20,100)
+    xpos = random.randrange(50,750)
+    ypos = random.randrange(50,750)
 
-print("did you click on the circle?", CircleCollision(x1, x2, y1, y2, radius))
+   
+    clock.tick(60) #FPS (frames per second)
+   
+    #pygame's way of listening for events (key presses, mouse clicks, etc)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           doExit = True #lets you quit program
+    #render section-----------------------------------
+    pygame.draw.circle(screen, (chief,keef,love), (xpos,ypos), sosa)
+    pygame.display.flip() 
 
+    
+    
+    
+#END GAME LOOP#######################################################
+pygame.quit()    
 
